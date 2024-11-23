4529c3a9d8019b061513dcd90cd48543d7b12e7e
Charlie Lamb <charlielamb20@icloud.com>
2024-11-23 14:18:49 +0000

    another cors fix??

--- packages/hono/lib/configure-cors.ts
+++ packages/hono/lib/configure-cors.ts
@@ -3,23 +3,19 @@ import { cors } from 'hono/cors'
 import { env } from '@dubble/env'
 
 export default function configureCors(app: AppOpenAPI) {
-  const origin = env.NEXT_PUBLIC_WEB.replace(/\/$/, '')
-
-  app.use(
-    '*',
-    cors({
-      origin: [origin],
-      allowHeaders: [
-        'Access-Control-Allow-Origin',
-        'Content-Type',
-        'Authorization',
-        'Accept',
-        'X-Requested-With',
-      ],
-      allowMethods: ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE', 'PATCH'],
-      exposeHeaders: ['Content-Length', 'Content-Type'],
-      maxAge: 600,
-      credentials: true,
-    })
-  )
+  const corsMiddleware = cors({
+    origin: [env.NEXT_PUBLIC_WEB],
+    allowHeaders: [
+      'Access-Control-Allow-Origin',
+      'Content-Type',
+      'Authorization',
+      'Accept',
+      'X-Requested-With',
+    ],
+    allowMethods: ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE', 'PATCH'],
+    exposeHeaders: ['Content-Length', 'Content-Type'],
+    maxAge: 600,
+    credentials: true,
+  })
+  app.use('*', corsMiddleware)
 }

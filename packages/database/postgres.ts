0a504afe6009026510f2d37ffa3e22eb4f9fdf57
Charlie Lamb <charlielamb20@icloud.com>
2024-11-23 12:23:49 +0000

    builds fixed??

--- packages/database/postgres.ts
+++ packages/database/postgres.ts
@@ -1,5 +1,7 @@
 import { env } from '@dubble/env'
+import { neon } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
 import * as schema from '@dubble/database/schema'
 
-export const db = drizzle(env.DATABASE_URL, { schema })
+const sql = neon(env.DATABASE_URL)
+export const db = drizzle(sql, { schema })

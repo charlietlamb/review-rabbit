e99af1ee8ee99adb6f3ac974932b52f82040ba92
Charlie Lamb <charlielamb20@icloud.com>
2024-12-06 19:46:11 +0000

    added gradient

--- packages/design-system/components/site/index/hero/hero-image.tsx
+++ packages/design-system/components/site/index/hero/hero-image.tsx
@@ -1,34 +1,24 @@
 'use client'
 
-import Image from 'next/image'
 import { motion } from 'framer-motion'
-import { env } from '@remio/env'
 
 export default function HeroImage() {
   return (
-    <div className="absolute left-0 right-0 -top-[300px] -bottom-20 -z-20 overflow-hidden">
+    <div className="absolute left-0 right-0 -top-[300px] -bottom-20 overflow-hidden">
       <motion.div
-        className="relative w-full h-full"
+        className="relative w-full h-full -z-20"
+        style={{
+          background: `radial-gradient(circle, hsla(var(--primary) / 0.2) 0%, hsla(var(--background) / 0.8) 50%, hsl(var(--background)) 100%)`,
+        }}
         animate={{
-          scale: [1, 1.02, 1],
+          scale: [1, 1.015, 1],
         }}
         transition={{
-          duration: 20,
+          duration: 15,
           repeat: Infinity,
           ease: 'easeInOut',
         }}
-      >
-        {/* <Image
-          alt="Hero Image"
-          src={`${env.NEXT_PUBLIC_AWS_CLOUDFRONT_URL}/public/hero-remio.webp`}
-          layout="fill"
-          objectFit="cover"
-          priority
-        /> */}
-        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-background" />
-      </motion.div>
-      {/* <div className="absolute inset-0 z-10" />
-      <div className="absolute inset-x-0 bottom-0 h-[100px] bg-gradient-to-t from-background to-transparent z-20" /> */}
+      />
     </div>
   )
 }

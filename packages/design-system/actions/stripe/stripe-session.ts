a05be166ba822632da52b053bf07efcd01c50ce2
Charlie Lamb <charlielamb20@icloud.com>
2024-12-26 18:20:34 +0000

    changing stripe redirect

--- packages/design-system/actions/stripe/stripe-session.ts
+++ packages/design-system/actions/stripe/stripe-session.ts
@@ -12,7 +12,8 @@ export const postStripeSession = async ({
   priceId: string
   plan: Plan
 }) => {
-  const successUrl = `${env.NEXT_PUBLIC_API}/stripe/subscription-success?session_id={CHECKOUT_SESSION_ID}`
+  // const successUrl = `${env.NEXT_PUBLIC_API}/stripe/subscription-success?session_id={CHECKOUT_SESSION_ID}`
+  const successUrl = `${env.NEXT_PUBLIC_WEB}/welcome?plan=${plan}`
   const cancelUrl = `${env.NEXT_PUBLIC_WEB}/cancel?session_id={CHECKOUT_SESSION_ID}`
 
   const session = await stripe.checkout.sessions.create({

66831acc08498166d05eeab5e958a6175433457f
Charlie Lamb <charlielamb20@icloud.com>
2025-01-05 14:41:06 +0000

    fixing build errors w dynamic route?

--- apps/web/app/(routes)/(auth)/verify/page.tsx
+++ apps/web/app/(routes)/(auth)/verify/page.tsx
@@ -1,6 +1,8 @@
 import { VerifyEmail } from '@rabbit/design-system/components/auth/verify/verify-email'
 import SiteLayout from '@rabbit/design-system/components/site/site-layout'
 
+export const dynamic = 'force-dynamic'
+
 export default async function VerifyPage() {
   return (
     <SiteLayout>

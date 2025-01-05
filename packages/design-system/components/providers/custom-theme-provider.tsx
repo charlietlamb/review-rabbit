3ba2514cb163a22d740efe075bdd9ed6b7b8dc25
Charlie Lamb <charlielamb20@icloud.com>
2025-01-05 14:52:36 +0000

    edit custom theme provider for attempted fix

--- packages/design-system/components/providers/custom-theme-provider.tsx
+++ packages/design-system/components/providers/custom-theme-provider.tsx
@@ -2,23 +2,30 @@
 
 import { useAtomValue } from 'jotai'
 import { customThemeAtom } from '@rabbit/design-system/atoms/dashboard/theme/custom-theme-atom'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function CustomThemeProvider({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const [mounted, setMounted] = useState(false)
   const theme = useAtomValue(customThemeAtom)
 
   useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  useEffect(() => {
+    if (!mounted) return
+
     const htmlElement = document.documentElement
     // Remove any existing theme classes
     const themeClasses = ['blue', 'red', 'orange', 'green', 'yellow', 'violet']
     htmlElement.classList.remove(...themeClasses)
     // Add new theme class
     htmlElement.classList.add(theme)
-  }, [theme])
+  }, [theme, mounted])
 
   return <>{children}</>
 }

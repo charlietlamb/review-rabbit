6c2b3bc70ac3a655a183bb47e81f7a5c0b34df7e
Charlie Lamb <charlielamb20@icloud.com>
2025-01-19 17:11:37 +0000

    further header adjustments

--- packages/design-system/components/dashboard/header/dashboard-header.tsx
+++ packages/design-system/components/dashboard/header/dashboard-header.tsx
@@ -4,14 +4,14 @@ import DashboardHeaderLocation from './dashboard-header-location'
 import Link from 'next/link'
 export default function DashboardHeader() {
   return (
-    <div className="md:h-16 h-10 border-b flex items-center p-2 gap-2 cursor-pointer hover:bg-primary/5 transition-all duration-600 px-3 group/header">
+    <header className="md:h-16 h-10 border-b flex items-center p-2 gap-2 cursor-pointer hover:bg-primary/5 transition-all duration-600 px-4 group/header">
       <Link href="/dashboard">
         <Rabbit className="min-w-8 min-h-8 fill-muted-foreground text-muted-foreground hover:text-foreground hover:fill-foreground transition-all duration-300" />
       </Link>
-      <Slash className="rotate-[-15deg] text-muted-foreground" />
+      <Slash className="rotate-[-30deg] text-muted-foreground/50" />
       <DashboardHeaderBusiness />
-      <Slash className="rotate-[-15deg] text-muted-foreground" />
+      <Slash className="rotate-[-30deg] text-muted-foreground/50" />
       <DashboardHeaderLocation />
-    </div>
+    </header>
   )
 }

89fe6935c6a2155d8313a8ec53bd74699ab2603b
Charlie Lamb <charlielamb20@icloud.com>
2025-01-20 22:51:36 +0000

    frontend up after api runtime changes

--- packages/design-system/components/providers/one-tap-provider.tsx
+++ packages/design-system/components/providers/one-tap-provider.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from 'next/navigation'
 import { env } from '@rabbit/env'
-import { getAuthClient } from '@rabbit/design-system/lib/auth-client'
+import { authClient } from '@rabbit/design-system/lib/auth-client'
 
 export default function OneTapProvider({
   children,
@@ -10,7 +10,6 @@ export default function OneTapProvider({
   children: React.ReactNode
 }) {
   if (env.NEXT_PUBLIC_DOMAIN === 'localhost') return <>{children}</>
-  const authClient = getAuthClient(env)
   const router = useRouter()
   const session = authClient.useSession()
   if (!session.data && !session.isPending) {
--- packages/design-system/lib/auth-client.ts
+++ packages/design-system/lib/auth-client.ts
@@ -1,14 +1,14 @@
 import { createAuthClient } from 'better-auth/react'
 import { inferAdditionalFields, oneTapClient } from 'better-auth/client/plugins'
 import { env } from '@rabbit/env'
-import { getAuth } from '@rabbit/auth'
+// import { getAuth } from '@rabbit/auth'
 
-const auth = getAuth(env)
+// const auth = getAuth(env)
 
 export const authClient = createAuthClient({
   baseURL: env.NEXT_PUBLIC_API,
   plugins: [
-    inferAdditionalFields<typeof auth>(),
+    // inferAdditionalFields<typeof auth>(),
     oneTapClient({
       clientId: env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
     }),

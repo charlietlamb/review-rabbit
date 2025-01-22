d8770e3e88283002db869e76443ed8d73ab89063
Charlie Lamb <charlielamb20@icloud.com>
2025-01-22 23:02:08 +0000

    remove isNotUser incorrect hook

--- packages/design-system/hooks/use-is-not-user.tsx
+++
@@ -1,9 +0,0 @@
-import { User } from '@rabbit/database/schema/users'
-import { redirect } from 'next/navigation'
-
-export default function useIsNotUser(user: User | null) {
-  if (user) {
-    return redirect('/dashboard')
-  }
-  return
-}

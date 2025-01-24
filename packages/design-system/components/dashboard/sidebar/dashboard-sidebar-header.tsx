006c86921d4990953c77571f88c771bf43c4fcf1
Charlie Lamb <charlielamb20@icloud.com>
2025-01-24 18:16:58 +0000

    update sidebar header hover

--- packages/design-system/components/dashboard/sidebar/dashboard-sidebar-header.tsx
+++ packages/design-system/components/dashboard/sidebar/dashboard-sidebar-header.tsx
@@ -1,15 +1,20 @@
 import { LogoSvg } from '@rabbit/design-system/components/site/header/logo-svg'
 import Link from 'next/link'
+import { useSidebar } from '@rabbit/design-system/components/ui/sidebar'
 
 export default function DashboardSidebarHeader() {
+  const { open } = useSidebar()
   return (
-    <div className="justify-center w-full h-16 flex items-center gap-2 p-4">
-      <Link href="/dashboard">
-        <LogoSvg className="min-w-8 min-h-8 fill-muted-foreground text-muted-foreground hover:text-foreground hover:fill-foreground transition-all duration-300" />
-      </Link>
-      <span className="font-heading text-md inline-block text-muted-foreground w-full font-bold">
-        Review Rabbit
-      </span>
-    </div>
+    <Link
+      href="/dashboard"
+      className="justify-center w-full h-16 flex items-center gap-2 p-4 group"
+    >
+      <LogoSvg className="min-w-8 min-h-8 fill-muted-foreground text-muted-foreground group-hover:text-foreground group-hover:fill-foreground transition-all duration-300" />
+      {open && (
+        <span className="font-heading text-md inline-flex text-muted-foreground w-full font-bold group-hover:text-foreground transition-all duration-300">
+          Review Rabbit
+        </span>
+      )}
+    </Link>
   )
 }
